322b6479bf52bb713a71448b7c56b9d09a4cbae4
仿生狮子 <1806234223@qq.com>
2026-09-01 12:46:09 +0800

    fix(components): code-block 补 pierre 主题模块类型声明

---
+++ packages/components/src/code-block/pierre-themes.d.ts
@@ -0,0 +1,9 @@
+declare module './pierre-dark-theme.js' {
+  const theme: Record<string, unknown>;
+  export default theme;
+}
+
+declare module './pierre-light-theme.js' {
+  const theme: Record<string, unknown>;
+  export default theme;
+}
